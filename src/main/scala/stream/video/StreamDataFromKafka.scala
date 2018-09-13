50b40b37f57162ef502369076ac03fa5b02dff83
HungLe <hunglmuit@gmail.com>
2018-09-13 16:19:19 +0700

    Finish insert, update, delete to jdbcPostgreSQL
    Signed-off-by: HungLe <hunglmuit@gmail.com>

--- src/main/scala/stream/video/StreamDataFromKafka.scala
+++ src/main/scala/stream/video/StreamDataFromKafka.scala
@@ -1,26 +1,104 @@
 package stream.video
 
+import java.sql.{DriverManager, Timestamp}
+
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.streaming.StreamingQuery
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField}
 import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
 
 object StreamDataFromKafka {
 
-  val dataSchema = List(
-    StructField("item_id", StringType, true),
-    StructField("title", StringType, true),
-    StructField("type", StringType, true),
-    StructField("publish", IntegerType, true),
-    StructField("publish_date", StringType, true),
-    StructField("episode_total", IntegerType, true),
-    StructField("episode_current", IntegerType, true),
-    StructField("last_video_add_date", StringType, true),
-    StructField("is_delete", IntegerType, true),
-    StructField("create_day", StringType, true)
-  )
+  def createSQLStatement(action:String,jsonNode:JsonNode, sysTime:String) {
+    //get val from jsonNode
+    println(action)
+    println(jsonNode)
+    ///////////////////////////////create connection to postgreSQL//////////////////////////////////////////////////////
+    val url = "jdbc:postgresql:streaming"
+    val user = "postgres"
+    val password = "dominic"
+    val conn=DriverManager.getConnection(url,user,password)
+    ////////////////////////////////add is_delete = 1 to video /////////////////////////////////////////////////////////
+    if(action == "delete"){
+      val itemId = jsonNode.path("_id").toString
+      val updateIsDeleteSQL =
+        """
+          |UPDATE video SET is_delete = 1 WHERE item_id = ?
+        """.stripMargin
+      val preparedStmt =conn.prepareStatement(updateIsDeleteSQL)
+      preparedStmt.setString(1,itemId)
+      preparedStmt.execute()
+      println("set is_delete for "+itemId)
+      preparedStmt.close()
+    }
+    else {
+      val itemId = jsonNode.path("_id").toString
+      val title = jsonNode.path("title").toString
+      val typeV = jsonNode.path("type").toString
+      val status = jsonNode.path("status").toString
+      val publish = status.toInt
+      val y = "1111-11-11 11:11:11.111"
+      var publishDate = Timestamp.valueOf(y)
+      val episodeTotal = jsonNode.path("episode_total").toString
+      val episodeCurrent = jsonNode.path("episode_current").toString
+      val lastVideoAndDate = jsonNode.path("last_video_add_date").toString
+      val tmpLastVideoAndDate = lastVideoAndDate.slice(1, lastVideoAndDate.length - 1)
+      val LastVideoAndDate = Timestamp.valueOf(tmpLastVideoAndDate)
+      ////////////////////////////////Insert into video (table)/////////////////////////////////////////////////////////
+      if (action == "insert") {
+        //create date = sysTime*/
+        val createDate = Timestamp.valueOf(sysTime)
 
+        //video already publish: publishDate =LastVideoAndDate
+        if (publish == 1) {
+          publishDate = LastVideoAndDate
+        }
+        //insert query
+        val insertSQL =
+          """
+            |insert into video (item_id,title,type,publish,publish_date,episode_total,episode_current,last_video_add_date,is_delete,create_date)
+            |values(?,?,?,?,?,?,?,?,?,?)
+          """.stripMargin
+        val preparedStmt = conn.prepareStatement(insertSQL)
+        preparedStmt.setString(1, itemId)
+        preparedStmt.setString(2, title)
+        preparedStmt.setString(3, typeV)
+        preparedStmt.setInt(4, publish)
+        preparedStmt.setTimestamp(5, publishDate)
+        preparedStmt.setInt(6, episodeTotal.toInt)
+        preparedStmt.setInt(7, episodeCurrent.toInt)
+        preparedStmt.setTimestamp(8, LastVideoAndDate)
+        preparedStmt.setInt(9, 0)
+        preparedStmt.setTimestamp(10, createDate)
+        preparedStmt.execute()
+        preparedStmt.close()
+      }////////////////////////////////Update into Video ///////////////////////////////////////////////////////////////
+      else if (action == "update") {
+        //get current status of publish
+        if (publish == 1) {
+          publishDate = Timestamp.valueOf(sysTime)
+        }
+        val updateSQL =
+          """
+            |UPDATE video SET  title = ?, type = ?, publish = ?, publish_date = ?, episode_total = ?, episode_current = ?, last_video_add_date = ?
+            |WHERE item_id = ?
+          """.stripMargin
+        val preparedStmt =conn.prepareStatement(updateSQL)
+        preparedStmt.setString(8, itemId)
+        preparedStmt.setString(1, title)
+        preparedStmt.setString(2, typeV)
+        preparedStmt.setInt(3, publish)
+        preparedStmt.setTimestamp(4, publishDate)
+        preparedStmt.setInt(5, episodeTotal.toInt)
+        preparedStmt.setInt(6, episodeCurrent.toInt)
+        preparedStmt.setTimestamp(7, LastVideoAndDate)
+        preparedStmt.execute()
+        preparedStmt.close()
+        println("updated for "+ itemId)
+      }
+    }
+    conn.close()
+  }
 
   def main (args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("StreamDataFromKafka").setMaster("local[3]")
@@ -69,68 +147,16 @@ object StreamDataFromKafka {
             val tmp=rawdata.slice(1,rawdata.length-1)
             val splitrawdata: Array[String] =tmp.split(",",4)
 
-            println(splitrawdata(2))
             //get topic, datetime and action
             val topic=splitrawdata(0)
             val datetime=splitrawdata(1)
             val action = splitrawdata(2)
-
+            //println(action)
             //parse data to json node
             val mapper = new ObjectMapper()
             val jnode: JsonNode =mapper.readTree(splitrawdata(3))
-            println(jnode)
-
-            //load data from postgreSQL by jdbc
-            val jdbcDF = spark.read
-              .format("jdbc")
-              .option("url", "jdbc:postgresql:streaming")
-              .option("dbtable", "video")
-              .option("user", "postgres")
-              .option("password", "dominic")
-              .load()
-
-            jdbcDF.show()
-
-            //working with insert/update/delete
-            if(action=="insert"){
-              //get from:
-              // _id    | title | type | status | episode_total | episode_current | last_video_and_date
-              //parse to:
-              //item_id | title | type | publish | *publish_date | episode_total | episode_current | last_video_and_date | *is_delete=0 | *create_date
-              println(jnode.path("_id"))
-              println(jnode.path("title"))
-              println(jnode.path("type"))
-              println(jnode.path("status"))
-              println(jnode.path("last_video_add_date"))
-              println(jnode.path("episode_current"))
-              println(jnode.path("episode_total"))
-
-            }else if(action=="update"){
-              // detect item_id in psql.
-              // update val
-              println(jnode.path("_id"))
-              println(jnode.path("title"))
-              println(jnode.path("type"))
-              println(jnode.path("status"))
-              println(jnode.path("last_video_add_date"))
-              println(jnode.path("episode_current"))
-              println(jnode.path("episode_total"))
-
-            }else if(action=="delete"){
-              // detect item_id in psql
-              // update is_delete=1
-              println(jnode.path("_id"))
-            }
-
-            // Saving data to a JDBC source
-            jdbcDF.write
-              .format("jdbc")
-              .option("url", "jdbc:postgresql:streaming")
-              .option("dbtable", "video")
-              .option("user", "postgres")
-              .option("password", "dominic")
-              .save()
-
+            val itemId = jnode.path("_id").toString
+            createSQLStatement(action,jnode,datetime)
           }
 
           override def close(errorOrNull: Throwable): Unit = {
--- src/main/scala/stream/video/jdbc_postgresql.scala
+++ src/main/scala/stream/video/jdbc_postgresql.scala
@@ -1,10 +1,11 @@
 package stream.video
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object jdbc_postgresql {
+
   def main (args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("jdbc_postgresql").setMaster("local[3]")
     val spark = SparkSession
@@ -20,46 +21,50 @@ object jdbc_postgresql {
       .option("password", "dominic")
       .load()
 
+
+
+
     jdbcDF.show()
-
-    //val data1: DataFrame = {"directors_detail":1,""}
-
-    jdbcDF.createOrReplaceTempView("video")
-    spark.sql("SELECT * FROM video").show()
 
     val dataSchema = List(
       StructField("item_id", StringType, true),
       StructField("title", StringType, true),
       StructField("type", StringType, true),
       StructField("publish", IntegerType, true),
-      StructField("publish_date", StringType, true),
+      StructField("publish_date", TimestampType, true),
       StructField("episode_total", IntegerType, true),
       StructField("episode_current", IntegerType, true),
-      StructField("last_video_add_date", StringType, true),
+      StructField("last_video_add_date", TimestampType, true),
       StructField("is_delete", IntegerType, true),
-      StructField("create_day", StringType, true)
+      StructField("create_day", TimestampType, true)
     )
 
     val someData = Seq(
-      Row("12a","title","movie",0,"2018-08-29",1,1,"2018-08-29",0,"2018-08-29"),
-      Row("12a","title","movie",0,"2018-08-29",1,1,"2018-08-29",0,"2018-08-29"),
-      Row("12a","title","movie",0,"2018-08-29",1,1,"2018-08-29",0,"2018-08-29")
+      Row("12abce","title","movie",0,"2018-08-29",1,1,"2018-08-29",1,"2018-08-29")
     )
     val newDF= spark.createDataFrame(
       spark.sparkContext.parallelize(someData),
       StructType(dataSchema)
     )
-    val dfjd= jdbcDF.join(newDF, usingColumn = "item_id")
+    newDF.show()
+//    val dfjd= jdbcDF.union(newDF)
+//    val dfjd = jdbcDF.joinWith(newDF, condition = "item_id")
+    //val dfjd= jdbcDF.join(newDF, usingColumn = "item_id")
 
-    // Saving data to a JDBC source
-    dfjd.write
-      .mode(SaveMode.ErrorIfExists)
+    jdbcDF.createOrReplaceTempView("video")
+
+    newDF.write
+      .insertInto("video")
+
+    /*// Saving data to a JDBC source
+    jdbcDF.write
+      .mode(SaveMode.Append)
       .format("jdbc")
       .option("url", "jdbc:postgresql:streaming")
       .option("dbtable", "video")
       .option("user", "postgres")
       .option("password", "dominic")
-      .save()
+      .save()*/
 
   }
 }
